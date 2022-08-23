85ff800f3879bbfb5e8d66e888d4a8de30d49b1e
Luka <luka.dimnik88@gmail.com>
2022-08-23 19:34:06 +0300

    Filter tabs script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('filters__active');
+    });
+    target.classList.add('filters__active');
 
+    tabs.forEach((tab) => {
+      tab.classList.remove('filter-tab-active');
+    });
+    tab.classList.add('filter-tab-active');
+  });
+});
 /*=============== DARK LIGHT THEME ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
